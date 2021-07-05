d20bcb5bf6a19cd4485cad3d92117174f6a3773a
pex <roman.ernst@farbenmeer.de>
2021-07-05 23:35:01 +0200

    Update VS code config

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,20 @@ echo "-- Cleaning up Brew"
 brew cleanup
 echo "-- Done."
 
+echo "-- Installing VS-Code extensions"
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension skyapps.fish-vscode
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension streetsidesoftware.code-spell-checker-german
+code --install-extension clinyong.vscode-css-modules
+code --install-extension GraphQL.vscode-graphql
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+echo "-- Done."
+
 echo "-- Installing English/International Keyboard Layout"
 wget -P ~/Library/Keyboard\ Layouts https://raw.githubusercontent.com/pex/keylayout-int-en-de/master/int-en-de.keylayout
 echo "-- Done."
@@ -37,4 +51,5 @@ echo "-- Done."
 echo "-- Linking hyper config"
 ln -hfs ~/.config/hyper/config.js ~/.hyper.js
 ln -hfs ~/.config/vscode/config.json ~/Library/Application\ Support/Code/User/settings.json
+ln -hfs ~/.config/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 echo "-- Done."
