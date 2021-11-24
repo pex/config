64b401a4e972084b19fa78f8b82141eceba31059
pex <roman.ernst@farbenmeer.de>
2021-11-24 15:10:44 +0100

    Introduce Brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,24 +6,8 @@ echo "-- Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "-- Done."
 
-echo "-- Installing Homebrew Formulas"
-brew install git
-brew install node
-brew install fish
-brew install yarn
-brew install tig
-brew install gnupg
-brew install wget --with-iri
-echo "-- Done."
-
-echo "-- Installing Cask Formulas"
-echo "-- (This might take some time...)"
-brew install --cask hyper
-brew install --cask visual-studio-code
-brew install --cask ngrok
-brew install --cask transmission
-brew install --cask spotify
-brew install --cask font-hack
+echo "-- Installing Homebrew bundle"
+brew bundle install
 echo "-- Done."
 
 echo "-- Cleaning up Brew"
@@ -31,7 +15,6 @@ brew cleanup
 echo "-- Done."
 
 echo "-- Installing VS-Code extensions"
-code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension skyapps.fish-vscode
@@ -39,7 +22,6 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-german
 code --install-extension clinyong.vscode-css-modules
 code --install-extension GraphQL.vscode-graphql
-code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
 echo "-- Done."
@@ -49,7 +31,7 @@ wget -P ~/Library/Keyboard\ Layouts https://raw.githubusercontent.com/pex/keylay
 echo "-- Done."
 
 echo "-- Linking hyper config"
-ln -hfs ~/.config/hyper/config.js ~/.hyper.js
+ln -hfs ~/.config/hyper/config.js ~/Library/Application\ Support/Hyper/.hyper.js
 ln -hfs ~/.config/vscode/config.json ~/Library/Application\ Support/Code/User/settings.json
 ln -hfs ~/.config/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 echo "-- Done."
